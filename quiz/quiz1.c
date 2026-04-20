60f3c35a60ab08e11e499b833bbef59aa932a201
shayankhannn <shayannkhannn007@gmail.com>
2026-04-20 08:12:51 -0700

    quiz and test

---
+++ quiz/quiz1.c
@@ -0,0 +1,16 @@
+// #include<stdio.h>
+// int main(void)
+// {   
+//     printf(‘ hello world\n’);
+//     return 0;
+// }    
+
+// find error 
+
+
+#include<stdio.h>
+int main(void)
+{   
+    printf(" hello world\n");
+    return 0;
+}    
